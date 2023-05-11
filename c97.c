f6d4ea58d66d462194df4065bd2391d38245243b
dhtmdgus <109403019+dhtmdgus@users.noreply.github.com>
2023-05-11 10:01:19 +0900

    c언어

---
+++ c97.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int num, encrypt, passwd, key=187;
+    printf("input 3 numbers: "), scanf("%d", &num);
+    encrypt=num^key;
+    printf("encrypt : %d\n", encrypt);
+    while(1){ 
+    printf("password : "), scanf("%d", &passwd);
+    printf("original data : %d\n", encrypt^passwd);
+    }
+
+    return 0;
+}
---
+++ myweb.html
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>메뉴1</title>
+</head>
+<body>
+	<a href="https://www.naver.com">메뉴1</a>
+</body>
+</html>
